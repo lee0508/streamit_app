c5e3898fd8f070acaef800b54977144ccdf12d0c
Lee dong hyeon <8503402+lee0508@users.noreply.github.com>
2022-07-08 17:08:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,4 @@ with st.sidebar:
 
 # image = Image.open('https://images.pexels.com/photos/12499889/pexels-photo-12499889.jpeg')
 
-st.markdown("[![Foo](http://www.google.com.au/images/nav_logo7.png)](http://google.com.au/)")
+st.markdown("[![Foo](https://images.pexels.com/photos/12499889/pexels-photo-12499889.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load)](http://google.com.au/)")
